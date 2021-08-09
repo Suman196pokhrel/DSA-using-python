4802656f574ac49b7f40922019bcf41e40921ab9
Suman pokhrel <spokhrel196@gmail.com>
2021-08-09 14:20:47 +0530

    Linked List Basic insertAfter peration added

--- linkedList/basicOperations.py
+++ linkedList/basicOperations.py
@@ -1,5 +1,5 @@
 class Node:
-     def __init__(self,data,next):
+     def __init__(self,data,next=None):
           self.data = data
           self.next = next
 
@@ -12,23 +12,50 @@ class LinkedList:
           node = Node(data,self.head)
           self.head = node
 
+     def insert_after(self,data,index):
+          if index > self.get_length() or index < 0:
+               raise Exception("The Given index is out or RangE")
+               
+          if index == 0:
+               node = Node(data,self.head.next)
+               self.head.next = node
+               return
 
-     def insert_at_middle(self,data,index):
-          pass
+          itr = self.head
+          for i in range(index):
+               itr = itr.next
+          
+          node = Node(data, itr.next)
+          itr.next = node
+
 
      def insert_at_end(self,data):
-          if self.head == None:
+          if self.head is None:
                print('LinkedList is Empty')
-               node = Node(data,None)
-               self.head = node
+               self.head = Node(data,None)
+               return
           
           
           itr = self.head
           
-          while itr.next != None:
+          while itr.next:
                itr = itr.next
 
           itr.next = Node(data,None)
+
+     def remove_at(self,index):
+          if index <0 or index >= self.get_length():
+               raise Exception("Not a valid INdex")
+          
+          if index == 0:
+               self.head = self.head.next
+               return
+
+          itr = self.head
+          for i in range(index-1):
+               itr = itr.next
+
+          itr.next = itr.next.next
 
      def print_data(self):
           if self.head == None:
@@ -43,6 +70,18 @@ class LinkedList:
                 
           print(llstr)
 
+     def createll_using_list(self,listData):
+          self.head = None
+          for data in listData:
+               self.insert_at_end(data)
+
+     def get_length(self):
+          itr = self.head
+          count = 0
+          while itr:
+               count +=1
+               itr = itr.next
+          return count
 
 if __name__ == '__main__':
      ll = LinkedList()
@@ -50,8 +89,27 @@ if __name__ == '__main__':
      ll.insert_at_beginning(12)
      ll.insert_at_beginning(13)
      ll.insert_at_beginning(14)
-
      ll.insert_at_end(22)
      ll.insert_at_end(25)
+     
 
+
+     # ll Using ListData 
+     # list1 = [22,33,44,55,66,77]
+     # ll.createll_using_list(listData=list1)
+     # ll.print_data()
+
+     # Length of LL 
+     # print(ll.get_length())
+
+
+     # ll.remove_at(3)
+     # ll.print_data()
+
+
+     ll.insert_after(133, 4)
      ll.print_data()
+
+
+
+
