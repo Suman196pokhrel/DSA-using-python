38c3da274a9ac7ef8bba7c9c59ddb41b6970a5ee
Suman_pokhrel <spokhrel196@gmail.com>
2021-08-14 09:32:36 +0530

    insert at start method fixed

--- linkedList/doublelinkedList/basicOperationsDll.py
+++ linkedList/doublelinkedList/basicOperationsDll.py
@@ -1,19 +1,41 @@
 
 
 class Node:
-     def __init__(self,prev=None,data=None,next=None):
-          self.prev = prev
+     def __init__(self,data):
+          self.prev = None
           self.data = data
-          self.next = next
+          self.next = None
      
 
 class doubleLinkedList:
      def __init__(self):
           self.head = None
+          self.tail = None
 
      def inser_at_start(self,data):
-          node = Node(None,data,self.head)
-          self.head = node
+          node = Node(data)
+          if self.head is None:
+               self.head = node
+               self.tail = node
+          
+          else:
+               self.head.prev = node
+               node.next = self.head
+               self.head = node
+               
+                   
+     def insert_at_end(self,data):
+          
+          # if self.head is None:
+          #      self.head = Node(data = data)
+          
+          # else:
+          #      temp = self.head
+          #      while temp.next != None:
+          #           temp = temp.next
+               
+          #      temp.next = Node()
+          pass
 
      def print_dllist(self):
           if self.head is None:
@@ -26,7 +48,7 @@ class doubleLinkedList:
                     values = values +'---->'+ str(itr.data)  
                     itr = itr.next 
 
-          print(values) 
+               print(values) 
 
      def get_length_dll(self):
           count = 0
@@ -41,15 +63,15 @@ class doubleLinkedList:
 if __name__ == '__main__':
      ll = doubleLinkedList()
      
-     ll.inser_at_start(data=11)
-     ll.inser_at_start(data=10)
-     ll.inser_at_start(data=9)
-     ll.inser_at_start(data=8)
-     ll.inser_at_start(data=7)
-     ll.inser_at_start(data=6)
+     ll.inser_at_start(11)
+     ll.inser_at_start(10)
+     ll.inser_at_start(9)
+     ll.inser_at_start(8)
+     ll.inser_at_start(7)
+     ll.inser_at_start(6)
 
-     print(ll.get_length_dll())
-     
+     # print(ll.get_length_dll())
+
      ll.print_dllist()
      
 
