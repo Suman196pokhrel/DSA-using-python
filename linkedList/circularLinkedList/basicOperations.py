d366dd8fa6849cac6c07d5e67cb6bf67bfed450f
Suman_pokhrel <spokhrel196@gmail.com>
2021-08-25 19:13:24 +0530

    basic ops on CLL

--- linkedList/circularLinkedList/basicOperations.py
+++ linkedList/circularLinkedList/basicOperations.py
@@ -1,29 +1,44 @@
 class Node:
-     def __init__(self,data):
-          self.data = data
-          self.next = None
+   def __init__(self, my_data):
+      self.data = my_data
+      self.next = None
 
-class CircularLinkedList:
-     def __init__(self):
-          self.head = None
-     
-     def insert_at_beginning(self,data):
-          node = Node(data)
-          temp = self.head
+class circularLinkedList:
+
+   def __init__(self):
+      self.head = None
 
 
+   def add_data(self, data):
+      new_node = Node(data) 
+      new_node.next = self.head
 
-     def insert_at_end(self,data):
-          pass
+      temp = self.head   
 
-     def printLList(self):
-          pass
+      if self.head is not None:
+         while(temp.next != self.head):
+            temp = temp.next
+         temp.next = new_node
+      else:
+         new_node.next = new_node
+      self.head = new_node
 
-     def getLength(self):
-          pass
 
+   def print_it(self):
+      temp = self.head
+      if self.head is not None:
+         while(True):
+            print("%d" %(temp.data)),
+            temp = temp.next
+            if (temp == self.head):
+               break
 
 if __name__ == '__main__':
-     cll = CircularLinkedList()
 
-     cll.printLList()
+     my_list = circularLinkedList()
+     print("Elements are added to the list ")
+     my_list.add_data (56)
+     my_list.add_data (78)
+     my_list.add_data (12)
+     print("The data is : ")
+     my_list.print_it()
