b68d6e33f8f66997ca9e3fd091f5ce99d28a8908
Suman_pokhrel <spokhrel196@gmail.com>
2021-08-28 00:19:40 +0530

    optimized method for balanced brackets

--- stack/hackerRankProblems/balancedBrackets.py
+++ stack/hackerRankProblems/balancedBrackets.py
@@ -8,41 +8,51 @@ from collections import deque
 # The function accepts STRING s as parameter.
 #
 
+# Not Optimized 
+# def isBalanced(s):
+#      stack = deque()
+#      top = None
+#      for item in s:
+#           # print(top,item,stack)
+#           if len(stack)==0:
+#                stack.append(item)
+#                top = stack[-1]
 
-def isBalanced(s):
-     stack = deque()
-     top = None
-     for item in s:
-          # print(top,item,stack)
-          if len(stack)==0:
-               stack.append(item)
-               top = stack[-1]
-
-          else:
-               top = stack[-1]
-               if top=='[':
-                    if item == ']':
-                         stack.pop()
-                    else:
-                         stack.append(item)
+#           else:
+#                top = stack[-1]
+#                if top=='[':
+#                     if item == ']':
+#                          stack.pop()
+#                     else:
+#                          stack.append(item)
                
-               elif top=='{':
-                    if item=='}':
-                         stack.pop()
-                    else:
-                         stack.append(item)
-               else:
-                    if item==')':
-                         stack.pop()
-                    else:
-                         stack.append(item)
+#                elif top=='{':
+#                     if item=='}':
+#                          stack.pop()
+#                     else:
+#                          stack.append(item)
+#                else:
+#                     if item==')':
+#                          stack.pop()
+#                     else:
+#                          stack.append(item)
     
-     if len(stack) == 0 :
-          return 'YES'
-     else:
-          return 'NO'      
+#      if len(stack) == 0 :
+#           return 'YES'
+#      else:
+#           return 'NO'      
           
-        
+def isBalanced(S):
+     stack = []
+     pairs = {"{": "}", "[": "]", "(" : ")"}
+     for i in S:
+          if not stack:
+               stack.append(i)
+          elif i == pairs.get(stack[-1]):
+               stack.pop()
+          else:
+               stack.append(i)
+     return "YES" if not stack else "NO"  
 
 if __name__ == '__main__':
 
